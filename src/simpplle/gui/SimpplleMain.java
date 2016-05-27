024e118b9b92664c01adeb3f42f0ba0342437223
michaelkinsey <dr.michael.kinsey@gmail.com>
2016-05-27 14:40:48 -0600

    Prepared release. Reverted gitignore issue.

--- src/simpplle/gui/SimpplleMain.java
+++ src/simpplle/gui/SimpplleMain.java
@@ -38,9 +38,9 @@ import java.util.Vector;
  */
 @SuppressWarnings("serial")
 public class SimpplleMain extends JFrame {
-  public static final String VERSION      = "1.2.3";
+  public static final String VERSION      = "1.2.4";
   public static final String RELEASE_KIND = "Douglas Fir";
-  public static final String BUILD_DATE   = "April 2016";
+  public static final String BUILD_DATE   = "May 2016";
 
   public static Color RESULT_COL_COLOR    = new Color(90,190,190);
   public static Color ROW_HIGHLIGHT_COLOR = new Color(162,200,157);
