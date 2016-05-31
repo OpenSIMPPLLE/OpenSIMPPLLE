8f4c0e41afeadd57c6b54c4c1e1eda36de1fbd91
Greg McMann <gregory.mcmann@umontana.edu>
2016-05-31 15:45:49 -0600

    VegSimStateData had documentation for a different class

--- src/simpplle/comcode/VegSimStateData.java
+++ src/simpplle/comcode/VegSimStateData.java
@@ -13,18 +13,17 @@ import org.hibernate.HibernateException;
 import java.io.*;
 
 /**
- * 
+ *
  * The University of Montana owns copyright of the designated documentation contained 
  * within this file as part of the software product designated by Uniform Resource Identifier 
  * UM-OpenSIMPPLLE-1.0.  By copying this file the user accepts the University of Montana
  * Open Source License Contract pertaining to this documentation and agrees to abide by all 
  * restrictions, requirements, and assertions contained therein.  All Other Rights Reserved.
+ * <p>
+ * VegSimStateData describes the state of an existing vegetation unit (EVU) during a single time step.
  *
- *<p>This class contains methods which define Vegetative Simulation States Keys.  This class has two methods, one which creates a 
- *string 2d array of keys of time [steps] [lifeformId's] and a method to get the keys. 
  * @author Documentation by Brian Losi
  * <p>Original source code authorship: Kirk A. Moeller
- * 
  *
  */
 public class VegSimStateData implements Externalizable {
