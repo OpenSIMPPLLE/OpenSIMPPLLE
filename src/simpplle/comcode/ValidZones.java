e4ff38e15bbd0bfce7df1c629279d9d89ff90e10
Greg McMann <gregory.mcmann@umontana.edu>
2016-06-01 09:24:51 -0600

    Tidied spacing in ValidZones

--- src/simpplle/comcode/ValidZones.java
+++ src/simpplle/comcode/ValidZones.java
@@ -7,8 +7,8 @@ package simpplle.comcode;
  * UM-OpenSIMPPLLE-1.0.  By copying this file the user accepts the University of Montana
  * Open Source License Contract pertaining to this documentation and agrees to abide by all 
  * restrictions, requirements, and assertions contained therein.  All Other Rights Reserved.
- *
- * <p>This interface contains constants to make and identifying the type of current zone used in several classes.
+ * <p>
+ * This interface contains constants to make and identifying the type of current zone used in several classes.
  * It contains all those zones currently supported in OpenSimpplle.
  * !!! Warning:  A change in these values will affect treatments. !!!
  *
@@ -21,23 +21,24 @@ package simpplle.comcode;
 
 public interface ValidZones {
 
-  public static final int WESTSIDE_REGION_ONE  = 0;
-  public static final int EASTSIDE_REGION_ONE  = 1;
-  public static final int SIERRA_NEVADA        = 2;
-  public static final int SOUTHERN_CALIFORNIA  = 3;
-  public static final int GILA                 = 4;
-  public static final int SOUTH_CENTRAL_ALASKA = 5;
-  public static final int SOUTHWEST_UTAH       = 6;
-  public static final int COLORADO_FRONT_RANGE = 7;
-  public static final int MICHIGAN             = 8;
-  public static final int ZONE_BUILDER         = 9;
+  public static final int WESTSIDE_REGION_ONE         = 0;
+  public static final int EASTSIDE_REGION_ONE         = 1;
+  public static final int SIERRA_NEVADA               = 2;
+  public static final int SOUTHERN_CALIFORNIA         = 3;
+  public static final int GILA                        = 4;
+  public static final int SOUTH_CENTRAL_ALASKA        = 5;
+  public static final int SOUTHWEST_UTAH              = 6;
+  public static final int COLORADO_FRONT_RANGE        = 7;
+  public static final int MICHIGAN                    = 8;
+  public static final int ZONE_BUILDER                = 9;
   public static final int WESTERN_GREAT_PLAINS_STEPPE = 10;
-  public static final int GREAT_PLAINS_STEPPE  = 11;
-  public static final int MIXED_GRASS_PRAIRIE  = 12;
-  public static final int COLORADO_PLATEAU     = 13;
-  public static final int TETON                = 14;
-  public static final int NORTHERN_CENTRAL_ROCKIES = 15;
-  public static final int GRASSLAND            = 16;
+  public static final int GREAT_PLAINS_STEPPE         = 11;
+  public static final int MIXED_GRASS_PRAIRIE         = 12;
+  public static final int COLORADO_PLATEAU            = 13;
+  public static final int TETON                       = 14;
+  public static final int NORTHERN_CENTRAL_ROCKIES    = 15;
+  public static final int GRASSLAND                   = 16;
+
 }
 
 
