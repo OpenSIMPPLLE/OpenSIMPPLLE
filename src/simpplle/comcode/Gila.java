4b7e23e27ad53d91be2a16331b141d0daf3c9b01
Greg McMann <gregory.mcmann@umontana.edu>
2016-06-10 14:13:06 -0600

    Cleaned up Gila

--- src/simpplle/comcode/Gila.java
+++ src/simpplle/comcode/Gila.java
@@ -1,8 +1,5 @@
 package simpplle.comcode;
 
-import java.util.Hashtable;
-
-
 /**
  * 
  * The University of Montana owns copyright of the designated documentation contained 
@@ -11,70 +8,62 @@ import java.util.Hashtable;
  * Open Source License Contract pertaining to this documentation and agrees to abide by all 
  * restrictions, requirements, and assertions contained therein.  All Other Rights Reserved.
  *
+ * <p> Gila describes the Gila National Forest located in New Mexico.
  *
- * <p>This class defines the zone for Gila, a Regional Zone object.
- * The primary purpose is to initialize class fields with values unique to Gila.  
- * As a result most member functions are private.
- * 
- * @author Documentation by Brian Losi
- * <p>Original source code authorship: Kirk A. Moeller
- * 
- * @see simpplle.comcode.RegionalZone
+ * <p> Original source code authorship: Kirk A. Moeller
  */
 
 public final class Gila extends RegionalZone {
-  private static final String arcviewDir  = "gis/gila";
-  private static final String homeDir = "knowledge/zones/gila";
-  private static final String gisFiles[] =
-    {"simpplle_arcview.apr", "process_legend.avl", "species_legend.avl",
-     "size_legend.avl", "canopy_legend.avl", "spread_legend.avl",
-     "probability_legend.avl", "spread_legend.avl", "treatment_legend.avl"};
+
+  private static final String arcviewDir = "gis/gila";
+  private static final String gisFiles[] = { "simpplle_arcview.apr",
+                                             "process_legend.avl",
+                                             "species_legend.avl",
+                                             "size_legend.avl",
+                                             "canopy_legend.avl",
+                                             "spread_legend.avl",
+                                             "probability_legend.avl",
+                                             "spread_legend.avl",
+                                             "treatment_legend.avl" };
 
   /**
-   * Constructor.  Inherits form Regional Zone superclass and initializes name, available = true, zoneDir, and the various jar file pathways. 
+   * Creates a regional zone for the Gila National Forest
    */
   public Gila () {
+
     super();
-    name      = "Gila";
-    available = true;
-    zoneDir = homeDir;
+
+    name            = "Gila";
+    available       = true;
+    zoneDir         = "knowledge/zones/gila";
     pathwayKnowFile = "zones/gila-pathways.jar";
-    sysKnowFile = "zones/gila.jar";
-    zoneDefnFile = "zones/gila-defn.jar";
-    gisExtraFile = "zones/gila-gis.jar";
+    sysKnowFile     = "zones/gila.jar";
+    zoneDefnFile    = "zones/gila-defn.jar";
+    gisExtraFile    = "zones/gila-gis.jar";
 
     probDataProcesses = new ProcessType[] {
-//      ProcessType.LIGHT_LP_MPB,a int representing 
-//      ProcessType.DF_BEETLE,
-//      ProcessType.ROOT_DISEASE,
-//      ProcessType.SPRUCE_BEETLE,
-//      ProcessType.WBP_MPB,
+      //ProcessType.LIGHT_LP_MPB,
+      //ProcessType.DF_BEETLE,
+      //ProcessType.ROOT_DISEASE,
+      //ProcessType.SPRUCE_BEETLE,
+      //ProcessType.WBP_MPB,
       ProcessType.PP_MPB
     };
-    createSampleAreas();
+
+    sampleAreas = new Area[1];
+    sampleAreas[0] = new Area("Gila","SAMPLE-AREAS/GILA.AREA",Area.SAMPLE);
+
   }
-/**
- * gets ArcView files at "gis/gila"
- */
+
   public String getArcviewDir() { return arcviewDir; }
-/**
- * Gets all the user probability processes for Gila zone.  
- */
+
   public ProcessType[] getUserProbProcesses() { return probDataProcesses; }
-/**
- * gets the GIS files at listed above 
- */
+
   protected String[] getGisFiles() { return gisFiles; }
 
-  private void createSampleAreas () {
-    sampleAreas    = new Area[1];
-    sampleAreas[0] = new Area("Gila","SAMPLE-AREAS/GILA.AREA",Area.SAMPLE);
-  }
-
   /**
-   * Gets the id of this zone.  Gila = 4
+   * @return The unique identifier for this zone
    * @see simpplle.comcode.ValidZones
-   * @return the id of this zone = 4
    */
   public int getId () {
     return ValidZones.GILA;
