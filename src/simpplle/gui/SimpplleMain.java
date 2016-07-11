525bb0cd91fe42fc528cea3f1a9b0e3a982b1909
michaelkinsey <dr.michael.kinsey@gmail.com>
2016-07-11 14:03:07 -0600

    Changed version string to 1.3.0

--- src/simpplle/gui/SimpplleMain.java
+++ src/simpplle/gui/SimpplleMain.java
@@ -33,9 +33,9 @@ import java.util.Vector;
  */
 @SuppressWarnings("serial")
 public class SimpplleMain extends JFrame {
-  public static final String VERSION      = "1.2.4";
+  public static final String VERSION      = "1.3.0";
   public static final String RELEASE_KIND = "Douglas Fir";
-  public static final String BUILD_DATE   = "May 2016";
+  public static final String BUILD_DATE   = "July 2016";
 
   public static Color RESULT_COL_COLOR    = new Color(90,190,190);
   public static Color ROW_HIGHLIGHT_COLOR = new Color(162,200,157);
