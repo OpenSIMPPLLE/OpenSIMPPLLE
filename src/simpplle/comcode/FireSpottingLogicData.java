1f197d1c8295c19eb053ec1693cbb3a72642af18
Greg McMann <gregory.mcmann@umontana.edu>
2016-07-18 14:38:32 -0600

    Removed class variable initialization from constructor

--- src/simpplle/comcode/FireSpottingLogicData.java
+++ src/simpplle/comcode/FireSpottingLogicData.java
@@ -53,8 +53,6 @@ public class FireSpottingLogicData extends LogicData implements Externalizable {
     ratStartDist = 0;
     endDist   = 0.0;
     ratEndDist = 0;
-    maxDist = -1.0;
-    ratMaxDist = -1;
     prob      = 0;
     
     sysKnowKind = SystemKnowledge.Kinds.FIRE_SPOTTING_LOGIC;
