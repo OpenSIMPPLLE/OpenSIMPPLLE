3f17d86d30e8d8db45b50effe31f96a69024cb6e
Greg McMann <gregory.mcmann@umontana.edu>
2016-07-20 14:51:56 -0600

    Added license to top of every Java file

--- src/simpplle/comcode/Lifeform.java
+++ src/simpplle/comcode/Lifeform.java
@@ -1,3 +1,11 @@
+/*
+ * The University of Montana owns copyright of the designated documentation contained
+ * within this file as part of the software product designated by Uniform Resource Identifier
+ * UM-OpenSIMPPLLE-1.0. By copying this file the user accepts the University of Montana
+ * Open Source License Contract pertaining to this documentation and agrees to abide by all
+ * restrictions, requirements, and assertions contained therein. All Other Rights Reserved.
+ */
+
 package simpplle.comcode;
 
 import java.beans.*;
