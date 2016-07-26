bddce01f4a777f0463f0c5bbfa4255055c1efd5e
Greg McMann <gregory.mcmann@umontana.edu>
2016-07-26 14:09:36 -0600

    Organized methods and updated documentation

--- src/simpplle/gui/LogicDataModel.java
+++ src/simpplle/gui/LogicDataModel.java
@@ -18,13 +18,8 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableColumn;
 
 /** 
- * This class has methods the Logic Data Model.  It is subclass of Abstract Table Model
- * rows in this table correspond to a the logic data kind.  Row count is achieved by counting the logic data kinds. 
- * 
- * @author Documentation by Brian Losi
- * <p>Original source code authorship: Kirk A. Moeller
- *
- * @see  javax.swing.table.AbstractTableModel
+ * LogicDataModel interrogates tabular data from system knowledge. The data interrogated by this
+ * class is displayed and manipulated within a JTable.
  */
 
 public class LogicDataModel extends AbstractTableModel {
@@ -35,10 +30,11 @@ public class LogicDataModel extends AbstractTableModel {
   private boolean isRegen = false;
 
   /**
-   * Constructor for Logic Data Model.  It takes in a string representing process kind, abstract base logic instance, and Jtable.
-   * @param kind
-   * @param logicInst
-   * @param table
+   * Creates a table model with a reference to model data and a table displaying the data.
+   *
+   * @param kind The name of a kind of knowledge
+   * @param logicInst A collection of knowledge
+   * @param table A table view
    */
   public LogicDataModel(String kind, AbstractBaseLogic logicInst, JTable table) {
     this.kindStr = kind;
@@ -47,107 +43,92 @@ public class LogicDataModel extends AbstractTableModel {
   }
 
   /**
-   * Gets the process kind in the Logic Data Model.
-   * @return
+   * Returns the kind of knowledge interrogated by this model.
    */
   public String getKind() {
     return kindStr;
   }
 
   /**
-   * Sets whether it is regeneration.  
-   * @param regen true if is regeneration.  
-   */
-  public void setIsRegen(boolean regen) {
-    isRegen = regen;
-  }
-
-  /**
-   * Gets the logic instance.  If it is a regeneration instance gets regeneration logic instance for the process.  Otherwise returns the logic instance for the data model.
-   * @return
+   * Returns the collection of knowledge interrogated by this model.
    */
   public AbstractBaseLogic getLogicInst() {
     return ( (isRegen) ? RegenerationLogic.getLogicInstance(kindStr) : logicInst);
   }
 
   /**
-   * This gets the column model for this logic data instance and then gets a particular column and returns the column identifier.  If the column identifier is a
-   * string it will get the column integer number from this string.
-   * @param col
-   * @return
+   *
    */
-  private int getRealColumnValue(int col) {
-    TableColumn column = table.getColumnModel().getColumn(col);
-    if (column.getIdentifier() instanceof String) {
-      return getLogicInst().getColumnNumFromName((String)column.getIdentifier());
-    }
-
-    return (Integer)column.getIdentifier();
+  public void setIsRegen(boolean regen) {
+    isRegen = regen;
   }
 
   /**
-   * Counts the number of visible columns for a particular logic data kind.
+   * Returns the most specific superclass for all the cell values in the column.
+   */
+  public Class getColumnClass(int columnIndex) {
+    if (getLogicInst().isDataPresent(kindStr)) {
+      return getValueAt(0, columnIndex).getClass();
+    }
+    return Object.class;
+  }
+
+  /**
+   * Returns the number of columns in the model.
    */
   public int getColumnCount() {
     return getLogicInst().getColumnCount(kindStr);
   }
 
   /**
-   * First checks to make sure there is data of the particular logic data kind.
+   * Returns the name of a column.
    */
-  public Class getColumnClass(int c) {
-    if (getLogicInst().isDataPresent(kindStr)) {
-      return getValueAt(0, c).getClass();
+  public String getColumnName(int columnIndex) {
+    String result = getLogicInst().getColumnName(kindStr,columnIndex);
+    return ((result == null) ? super.getColumnName(columnIndex) : result);
+  }
+
+  /**
+   * Returns an adjusted column index.
+   */
+  private int getRealColumnValue(int col) {
+    TableColumn column = table.getColumnModel().getColumn(col);
+    if (column.getIdentifier() instanceof String) {
+      return getLogicInst().getColumnNumFromName((String)column.getIdentifier());
     }
-    return Object.class;
+    return (Integer)column.getIdentifier();
   }
 
   /**
-   * If is a regeneration logic data instance and the column default 0 column or past the base logic columns will return false, else true.  Otherwise if
-   * the column is anything but the default 0 column will return true.
+   * Shows a hidden column.
    */
-  public boolean isCellEditable(int row, int col) {
-    col = getRealColumnValue(col);
-    if (isRegen) {
-      return (col != getLogicInst().ROW_COL &&
-              col != RegenerationData.SPECIES_CODE_COL);
-    } else {
-      return (col != getLogicInst().ROW_COL);
-    }
+  public void addVisibleColumn(int columnIndex) {
+    getLogicInst().addVisibleColumn(kindStr,columnIndex);
   }
 
   /**
-   * Gets the value of an object at a particular cell (row and column entered as parameter).
-   *
+   * Hides a visible column.
    */
-  public Object getValueAt(int row, int col) {
-    col = getRealColumnValue(col);
-    if (col == getLogicInst().ROW_COL) { return row; }
-    Object o = getLogicInst().getValueAt(row,col,kindStr);
-    return o;
+  public void removeVisibleColumn(int columnIndex) {
+    getLogicInst().removeVisibleColumn(kindStr,columnIndex);
   }
 
   /**
-   *
-   * @param row
-   * @return Gets the value of a particular row based on the logic data instance and kind.
+   * Returns an array of visible column indices.
    */
-  public AbstractLogicData getValueAt(int row) {
-    return getLogicInst().getValueAt(row,kindStr);
+  public int[] getVisibleColumns() {
+    return getLogicInst().getVisibleColumns(kindStr);
   }
 
   /**
-   * Sets the object in a particular cell.  Then notifies all listeners for the table that that particular cell has been updated.
-   *
+   * Returns true if the column is visible.
    */
-  public void setValueAt(Object value, int row, int col) {
-    col = getRealColumnValue(col);
-    getLogicInst().setData(value,row,col,kindStr);
-    fireTableCellUpdated(row,col);
+  public boolean isVisibleColumn(int columnIndex) {
+    return getLogicInst().isVisibleColumn(kindStr,columnIndex);
   }
 
   /**
-   * Gets the current logic data instance.  If it is not null gets the row count by counting the size of the data kind arraylist.  
+   * Returns the number of rows in the model.
    */
   public int getRowCount() {
     AbstractBaseLogic inst = getLogicInst();
@@ -155,8 +136,16 @@ public class LogicDataModel extends AbstractTableModel {
   }
 
   /**
-   * Adds a row and then notifies all listeners for the table that a row has been added between the 0th row and the Nth row.
-   * @param insertPos the position where the row will be inserted
+   * Returns a single row of logic data.
+   */
+  public AbstractLogicData getValueAt(int rowIndex) { // Rename to getRowData
+    return getLogicInst().getValueAt(rowIndex,kindStr);
+  }
+
+  /**
+   * Adds a row of logic data and forwards the change to listeners.
+   *
+   * @param insertPos The index where the new row will be placed
    */
   public void addRow(int insertPos) {
     getLogicInst().addRow(insertPos,kindStr);
@@ -165,95 +154,100 @@ public class LogicDataModel extends AbstractTableModel {
   }
 
   /**
-   * Duplicates a row by taking in the row, and insertion position.  
-   * Then notifies all listeners for the table that a row has been duplicated  between the 0th row and the Nth row.
-   * @param row
-   * @param insertPos
+   * Duplicates a row of logic data and forwards the change to listeners.
+   *
+   * @param rowIndex The row to be duplicated
+   * @param insertPos The index where the duplicate will be placed
    */
-  public void duplicateRow(int row, int insertPos) {
-    getLogicInst().duplicateRow(row,insertPos,kindStr);
+  public void duplicateRow(int rowIndex, int insertPos) {
+    getLogicInst().duplicateRow(rowIndex,insertPos,kindStr);
     fireTableRowsInserted(0, getRowCount());
     fireTableDataChanged();
   }
 
   /**
-   * Deletes a particular row.
-   * @param row the row to be deleted.  
+   * Deletes a row of logic data and forwards the change to listeners.
+   *
+   * @param rowIndex The row to be deleted
    */
-  public void deleteRow(int row) {
-    getLogicInst().removeRow(row,kindStr);
-    fireTableRowsDeleted(row,row);
+  public void deleteRow(int rowIndex) {
+    getLogicInst().removeRow(rowIndex,kindStr);
+    fireTableRowsDeleted(rowIndex,rowIndex);
   }
 
   /**
-   * Moves a row upward.  Then notifies listeners for a table that all cells in the table have been changed.  
-   * @param row the row to be moved upward
-   * @return the integer location of the new row
+   * Decrements the index of a row and forwards the change to listeners.
+   *
+   * @param rowIndex The row whose index will be decremented
+   * @return The new row index
    */
-  public int moveRowUp(int row) {
-    int newRow = getLogicInst().moveRowUp(row,kindStr);
+  public int moveRowUp(int rowIndex) {
+    int newIndex = getLogicInst().moveRowUp(rowIndex,kindStr);
     fireTableDataChanged();
-    return newRow;
+    return newIndex;
   }
 
   /**
-   * Moves a row downward.  Then notifies listeners for a table that all cells in the table have been changed.  
-   * @param row the number of row to be moved downward
-   * @return the integer location of the new row
+   * Increments the index of a row and forwards the change to listeners.
+   *
+   * @param rowIndex The row whose index will be incremented
+   * @return The new row index
    */
-  public int moveRowDown(int row) {
-    int newRow = getLogicInst().moveRowDown(row,kindStr);
+  public int moveRowDown(int rowIndex) {
+    int newIndex = getLogicInst().moveRowDown(rowIndex,kindStr);
     fireTableDataChanged();
-    return newRow;
+    return newIndex;
   }
 
   /**
-   * Gets the string literal column name from the integer value of the column.  If it is null will return the abstract table model column name, if not returns
-   * the logical instance column name.  
+   * Returns the value for the cell at columnIndex and rowIndex.
+   *
+   * @param rowIndex The row whose value is to be queried
+   * @param columnIndex The column whose value is to be queried
+   * @return The value at the specified cell
    */
-  public String getColumnName(int column) {
-    String result = getLogicInst().getColumnName(kindStr,column);
-    return ((result == null) ? super.getColumnName(column) : result);
+  public Object getValueAt(int rowIndex, int columnIndex) {
+    columnIndex = getRealColumnValue(columnIndex);
+    if (columnIndex == getLogicInst().ROW_COL) { return rowIndex; }
+    Object o = getLogicInst().getValueAt(rowIndex,columnIndex,kindStr);
+    return o;
   }
 
   /**
-   * Checks to see if there is data present in Logic Data Model.  This will be true if either this is regeneration data or instance data
-   * @return true if data is present.
+   * Sets the value in the cell at columnIndex and rowIndex.
+   *
+   * @param value The new value
+   * @param rowIndex The row whose value is to be changed
+   * @param columnIndex The column whose value is to be changed
+   */
+  public void setValueAt(Object value, int rowIndex, int columnIndex) {
+    columnIndex = getRealColumnValue(columnIndex);
+    getLogicInst().setData(value,rowIndex,columnIndex,kindStr);
+    fireTableCellUpdated(rowIndex,columnIndex);
+  }
+
+  /**
+   * Returns true if the cell at rowIndex and columnIndex is editable.
+   *
+   * @param rowIndex The row whose value is to be queried
+   * @param columnIndex The column whose value is to be queried
+   * @return True if the cell is editable
+   */
+  public boolean isCellEditable(int rowIndex, int columnIndex) {
+    columnIndex = getRealColumnValue(columnIndex);
+    if (isRegen) {
+      return (columnIndex != getLogicInst().ROW_COL &&
+          columnIndex != RegenerationData.SPECIES_CODE_COL);
+    } else {
+      return (columnIndex != getLogicInst().ROW_COL);
+    }
+  }
+
+  /**
+   * Returns true if logic data is present for the model's kind of knowledge.
    */
   public boolean isDataPresent() {
     return getLogicInst().isDataPresent(kindStr);
-  }
-
-  /**
-   * This method is called with the correct column value.  It adds a visible column to the data model.  
-   * @param col column # to be added. 
-   */
-  public void addVisibleColumn(int col) {
-    getLogicInst().addVisibleColumn(kindStr,col);
-  }
-
-  /**
-   * This method is called with the correct column value.  It removes a visible column to the data model.  
-   * @param col column # to be removed. 
-   */
-  public void removeVisibleColumn(int col) {
-    getLogicInst().removeVisibleColumn(kindStr,col);
-  }
-
-  /**
-   * Gets an array of all the visible column.  
-   * @return
-   */
-  public int[] getVisibleColumns() {
-    return getLogicInst().getVisibleColumns(kindStr);
-  }
-
-  /**
-   * This method is called with the correct column value.  It checks if the column number corresponds to a visible column..  
-   * @param col column # to bechecked for visibilty. 
-   */
-  public boolean isVisibleColumn(int col) {
-    return getLogicInst().isVisibleColumn(kindStr,col);
   }
 
 }
