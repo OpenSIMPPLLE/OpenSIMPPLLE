44d7ce24037d320bbed0c12f61dd6a23018597bd
Greg McMann <gregory.mcmann@umontana.edu>
2016-08-24 14:14:44 -0600

    Incremented version to 1.3.3

--- src/main/simpplle/gui/SimpplleMain.java
+++ src/main/simpplle/gui/SimpplleMain.java
@@ -33,7 +33,7 @@ import java.util.Vector;
  */
 @SuppressWarnings("serial")
 public class SimpplleMain extends JFrame {
-  public static final String VERSION      = "1.3.2";
+  public static final String VERSION      = "1.3.3";
   public static final String RELEASE_KIND = "Douglas Fir";
   public static final String BUILD_DATE   = "August 2016";
 
