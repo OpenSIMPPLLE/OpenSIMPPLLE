4db54e61ef5722f4c8a3f3c26791a9f4391e3dba
Greg McMann <gregory.mcmann@umontana.edu>
2016-10-11 13:37:04 -0600

    Incremented version to 1.3.6

--- src/main/simpplle/gui/SimpplleMain.java
+++ src/main/simpplle/gui/SimpplleMain.java
@@ -33,9 +33,9 @@ import java.util.Vector;
  */
 @SuppressWarnings("serial")
 public class SimpplleMain extends JFrame {
-  public static final String VERSION      = "1.3.5";
+  public static final String VERSION      = "1.3.6";
   public static final String RELEASE_KIND = "Douglas Fir";
-  public static final String BUILD_DATE   = "September 2016";
+  public static final String BUILD_DATE   = "October 2016";
 
   public static Color RESULT_COL_COLOR    = new Color(90,190,190);
   public static Color ROW_HIGHLIGHT_COLOR = new Color(162,200,157);
