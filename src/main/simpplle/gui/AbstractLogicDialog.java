c4dc4a8569e95dd322212bb034fdc0dbfba5f0ad
Kyle <kyle.lucke@umt.edu>
2017-05-24 11:23:45 -0600

    Cleaned up AbstractLogicDialogue class. Replaced implemented interfaces with method references. This looks much cleaner and is more efficient. Global Variables were also localized where possible.

--- src/main/simpplle/gui/AbstractLogicDialog.java
+++ src/main/simpplle/gui/AbstractLogicDialog.java
@@ -14,10 +14,8 @@ import simpplle.comcode.SystemKnowledge;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.io.File;
 
@@ -30,33 +28,17 @@ import java.io.File;
  */
 
 public class AbstractLogicDialog extends JDialog {
+
   protected String[]              panelKinds;
   protected String                currentPanelKind;
   protected BaseLogicPanel         currentPanel;
   protected BaseLogicPanel[]       tabPanels;
   protected SystemKnowledge.Kinds sysKnowKind;
 
-  protected JPanel mainPanel = new JPanel();
-  protected BorderLayout borderLayout1 = new BorderLayout();
-  protected JPanel tabsPanel = new JPanel();
-  protected BorderLayout borderLayout2 = new BorderLayout();
+  protected JPanel mainPanel = new JPanel(new BorderLayout());
   protected JTabbedPane tabbedPane = new JTabbedPane();
-  protected JMenuBar menuBar = new JMenuBar();
-  protected JMenu menuFile = new JMenu();
-  protected JMenuItem menuFileOpen = new JMenuItem();
   protected JMenuItem menuFileClose = new JMenuItem();
   protected JMenuItem menuFileSave = new JMenuItem();
-  protected JMenuItem menuFileSaveAs = new JMenuItem();
-  protected JMenuItem menuFileLoadDefault = new JMenuItem();
-  protected JMenuItem menuFileQuit = new JMenuItem();
-  protected JMenu menuAction = new JMenu();
-  protected JMenuItem menuActionMoveRuleUp = new JMenuItem();
-  protected JMenuItem menuActionMoveRuleDown = new JMenuItem();
-  protected JMenuItem menuActionInsertNewRule = new JMenuItem();
-  protected JMenuItem menuActionDeleteSelectedRule = new JMenuItem();
-  protected JMenuItem menuActionDuplicateSelectedRule = new JMenuItem();
-  protected JMenu menuKnowledgeSource = new JMenu();
-  protected JMenuItem menuKnowledgeSourceEdit = new JMenuItem();
 
   /**
    * Constructor which calls jbInit() to start abstract logic UI
@@ -73,63 +55,48 @@ public class AbstractLogicDialog extends JDialog {
       ex.printStackTrace();
     }
   }
-  
-  
+
   public AbstractLogicDialog() {
     this(new Frame(), "LogicDialog", false);
   }
-
   /**
    * 
    * @throws Exception
    */
   private void jbInit() throws Exception {
 
-    mainPanel.setLayout(borderLayout1);
-    tabsPanel.setLayout(borderLayout2);
+    JPanel tabsPanel = new JPanel(new BorderLayout());
+    JTabbedPane tabbedPane = new JTabbedPane();
+    JMenuBar menuBar = new JMenuBar();
 
-    menuFile.setText("File");
-    menuFileOpen.setText("Open");
-    menuFileOpen.addActionListener(new
-        AbstractLogicDialog_menuFileOpen_actionAdapter(this));
+    JMenu menuFile = new JMenu("File");
+    JMenuItem menuFileOpen = new JMenuItem("Open");
+    menuFileOpen.addActionListener(this::open);
     menuFileClose.setText("Close");
-    menuFileClose.addActionListener(new
-        AbstractLogicDialog_menuFileClose_actionAdapter(this));
+    menuFileClose.addActionListener(this::close);
     menuFileSave.setText("Save");
-    menuFileSave.addActionListener(new
-        AbstractLogicDialog_menuFileSave_actionAdapter(this));
-    menuFileSaveAs.setText("Save As");
-    menuFileSaveAs.addActionListener(new
-        AbstractLogicDialog_menuFileSaveAs_actionAdapter(this));
-    menuFileLoadDefault.setText("Load Default");
-    menuFileLoadDefault.addActionListener(new
-        AbstractLogicDialog_menuFileLoadDefault_actionAdapter(this));
-    menuFileQuit.setText("Close Dialog");
-    menuFileQuit.addActionListener(new
-        AbstractLogicDialog_menuFileQuit_actionAdapter(this));
-
-    menuAction.setText("Action");
-    menuActionMoveRuleUp.setText("Move Rule Up");
-    menuActionMoveRuleUp.addActionListener(new
-        AbstractLogicDialog_menuActionMoveRuleUp_actionAdapter(this));
-    menuActionMoveRuleDown.setText("Move Rule Down");
-    menuActionMoveRuleDown.addActionListener(new
-        AbstractLogicDialog_menuActionMoveRuleDown_actionAdapter(this));
-    menuActionInsertNewRule.setText("Insert New Rule");
-    menuActionInsertNewRule.addActionListener(new
-        AbstractLogicDialog_menuActionInsertNewRule_actionAdapter(this));
-    menuActionDeleteSelectedRule.setText("Delete Selected Rule");
-    menuActionDeleteSelectedRule.addActionListener(new
-        AbstractLogicDialog_menuActionDeleteSelectedRule_actionAdapter(this));
-    menuActionDuplicateSelectedRule.setText("Duplicate Selected Rule");
-    menuActionDuplicateSelectedRule.addActionListener(new
-        AbstractLogicDialog_menuActionDuplicateSelectedRule_actionAdapter(this));
-    tabbedPane.addChangeListener(new
-        AbstractLogicDialog_tabbedPane_changeAdapter(this));
-    menuKnowledgeSource.setText("Knowledge Source");
-    menuKnowledgeSourceEdit.setText("Display/Edit");
-    menuKnowledgeSourceEdit.addActionListener(new
-      AbstractLogicDialog_menuKnowledgeSourceEdit_actionAdapter(this));
+    menuFileSave.addActionListener(this::save);
+    JMenuItem menuFileSaveAs = new JMenuItem("Save As");
+    menuFileSaveAs.addActionListener(this::saveAs);
+    JMenuItem menuFileLoadDefault = new JMenuItem("Load Default");
+    menuFileLoadDefault.addActionListener(this::loadDefault);
+    JMenuItem menuFileQuit = new JMenuItem("Close Dialog");
+    menuFileQuit.addActionListener(this::quit);
+    JMenu menuAction = new JMenu("Action");
+    JMenuItem menuActionMoveRuleUp = new JMenuItem("Move Rule Up");
+    menuActionMoveRuleUp.addActionListener(this::moveRuleUp);
+    JMenuItem menuActionMoveRuleDown = new JMenuItem("Move Rule Down");
+    menuActionMoveRuleDown.addActionListener(this::moveRuleDown);
+    JMenuItem menuActionInsertNewRule = new JMenuItem("Insert New Rule");
+    menuActionInsertNewRule.addActionListener(this::insertNewRule);
+    JMenuItem menuActionDeleteSelectedRule = new JMenuItem("Delete Selected Rule");
+    menuActionDeleteSelectedRule.addActionListener(this::deleteSelectedRule);
+    JMenuItem menuActionDuplicateSelectedRule = new JMenuItem("Duplicate Selected Rule");
+    menuActionDuplicateSelectedRule.addActionListener(this::duplicateSelectedRule);
+    tabbedPane.addChangeListener(this::tabbedPane_stateChanged);
+    JMenu menuKnowledgeSource = new JMenu("Knowledge Source");
+    JMenuItem menuKnowledgeSourceEdit = new JMenuItem("Display/Edit");
+    menuKnowledgeSourceEdit.addActionListener(this::menuKnowledgeSourceEdit);
 
     getContentPane().add(mainPanel);
     mainPanel.add(tabsPanel, java.awt.BorderLayout.CENTER);
@@ -191,7 +158,6 @@ public class AbstractLogicDialog extends JDialog {
     //updateDialog();
 
   }
-
   /**
    * Updates the Abstract Logic Dialog.
    */
@@ -211,7 +177,7 @@ public class AbstractLogicDialog extends JDialog {
    * If open is selected from JMenu, will open the file containing this type of 
    * @param e
    */
-  public void menuFileOpen_actionPerformed(ActionEvent e) {
+  public void open(ActionEvent e) {
     SystemKnowledgeFiler.openFile(this,sysKnowKind,menuFileSave,menuFileClose);
     //  This line must be done first!
     for (int i=0; i<tabPanels.length; i++) {
@@ -225,7 +191,6 @@ public class AbstractLogicDialog extends JDialog {
     currentPanel.updateDialog();
     updateDialog();
   }
-  
   /**
    * Loads default logic from System Knowledge class.  
    * @throws SimpplleError catches the error thrown from SystemKnowledge loadZoneKnowledge
@@ -235,7 +200,7 @@ public class AbstractLogicDialog extends JDialog {
       String msg = "This will load the default Logic.\n\n" +
                    "Do you wish to continue?";
       String title = "Load Default Logic";
-    
+
       if (Utility.askYesNoQuestion(this,msg,title)) {
         SystemKnowledge.loadZoneKnowledge(sysKnowKind);
         //  This line must be done first!
@@ -253,14 +218,13 @@ public class AbstractLogicDialog extends JDialog {
                                     JOptionPane.ERROR_MESSAGE);
     }
   }
-/**
- * If close is choosen from menu file close loads defaults.  
- * @param e
- */
-  public void menuFileClose_actionPerformed(ActionEvent e) {
+  /**
+   * If close is choosen from menu file close loads defaults.
+   * @param e
+   */
+  public void close(ActionEvent e) {
     loadDefaults();
   }
-
   /**
    * Checks if save file menu should be enabled.  
    * @return true if System Knowledge has a file and system knowledge changed or has user data input.  
@@ -274,57 +238,56 @@ public class AbstractLogicDialog extends JDialog {
    * @param e save menu item selected
    * @see SystemKnowledgeFiler()
    */
-  public void menuFileSave_actionPerformed(ActionEvent e) {
+  public void save(ActionEvent e) {
     File outfile = SystemKnowledge.getFile(sysKnowKind);
     SystemKnowledgeFiler.saveFile(this,outfile,sysKnowKind,menuFileSave,menuFileClose);
   }
-/**
- * 'Save as' function sends to overloaded SystemKnowledgeFiler.saveFile method.   
- * @param e Save As menu item selected
- */
-  public void menuFileSaveAs_actionPerformed(ActionEvent e) {
+  /**
+   * 'Save as' function sends to overloaded SystemKnowledgeFiler.saveFile method.
+   * @param e Save As menu item selected
+   */
+  public void saveAs(ActionEvent e) {
     SystemKnowledgeFiler.saveFile(this,sysKnowKind,menuFileSave,menuFileClose);
   }
 
-  public void menuFileLoadDefault_actionPerformed(ActionEvent e) {
+  private void loadDefault(ActionEvent e) {
     loadDefaults();
   }
 
-  public void menuFileQuit_actionPerformed(ActionEvent e) {
+  public void quit(ActionEvent e) {
     setVisible(false);
   }
 
-  public void menuActionMoveRuleUp_actionPerformed(ActionEvent e) {
+  private void moveRuleUp(ActionEvent e) {
     currentPanel.moveRowUp();
     updateDialog();
   }
 
-  public void menuActionMoveRuleDown_actionPerformed(ActionEvent e) {
+  private void moveRuleDown(ActionEvent e) {
     currentPanel.moveRowDown();
     updateDialog();
   }
-/**
- * Inserts a new row in the currentPanel.  
- * @param e "Insert New Rule" menu item selected
- */
-  public void menuActionInsertNewRule_actionPerformed(ActionEvent e) {
+  /**
+   * Inserts a new row in the currentPanel.
+   * @param e "Insert New Rule" menu item selected
+   */
+  private void insertNewRule(ActionEvent e) {
     currentPanel.insertRow();
     updateDialog();
   }
-
   /**
    * Deletes selected rule containing row.  
    * @param e "Delete Selected Rule" selected
    */
-  public void menuActionDeleteSelectedRule_actionPerformed(ActionEvent e) {
+  private void deleteSelectedRule(ActionEvent e) {
     currentPanel.deleteSelectedRow();
     updateDialog();
   }
-  public void menuActionDuplicateSelectedRule_actionPerformed(ActionEvent e) {
+
+  private void duplicateSelectedRule(ActionEvent e) {
     currentPanel.duplicateSelectedRow();
     updateDialog();
   }
-  
   /**
    * 
    * @param e
@@ -340,7 +303,7 @@ public class AbstractLogicDialog extends JDialog {
     updateDialog();
   }
 
-  public void menuKnowledgeSourceEdit_actionPerformed(ActionEvent e) {
+  public void menuKnowledgeSourceEdit(ActionEvent e) {
     String str = SystemKnowledge.getKnowledgeSource(sysKnowKind);
     String title = "Knowledge Source";
 
@@ -351,174 +314,5 @@ public class AbstractLogicDialog extends JDialog {
     if (newKnowledge != null) {
       SystemKnowledge.setKnowledgeSource(sysKnowKind,newKnowledge);
     }
-  }
-
-}
-
-class AbstractLogicDialog_tabbedPane_changeAdapter implements
-    ChangeListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_tabbedPane_changeAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void stateChanged(ChangeEvent e) {
-    adaptee.tabbedPane_stateChanged(e);
-  }
-}
-
-class AbstractLogicDialog_menuActionDeleteSelectedRule_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuActionDeleteSelectedRule_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuActionDeleteSelectedRule_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuActionDuplicateSelectedRule_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuActionDuplicateSelectedRule_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuActionDuplicateSelectedRule_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuActionInsertNewRule_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuActionInsertNewRule_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuActionInsertNewRule_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuActionMoveRuleDown_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuActionMoveRuleDown_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuActionMoveRuleDown_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuActionMoveRuleUp_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuActionMoveRuleUp_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuActionMoveRuleUp_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuFileQuit_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuFileQuit_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuFileQuit_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuFileLoadDefault_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuFileLoadDefault_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuFileLoadDefault_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuFileSaveAs_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuFileSaveAs_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuFileSaveAs_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuFileSave_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuFileSave_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuFileSave_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuFileClose_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuFileClose_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuFileClose_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuFileOpen_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuFileOpen_actionAdapter(
-      AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuFileOpen_actionPerformed(e);
-  }
-}
-
-class AbstractLogicDialog_menuKnowledgeSourceEdit_actionAdapter implements
-    ActionListener {
-  private AbstractLogicDialog adaptee;
-  AbstractLogicDialog_menuKnowledgeSourceEdit_actionAdapter(AbstractLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuKnowledgeSourceEdit_actionPerformed(e);
   }
 }
