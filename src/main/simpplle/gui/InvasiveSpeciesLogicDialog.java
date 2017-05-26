2af7a48d3337e850447bb54025e4f00ab0c6df91
Kyle <kyle.lucke@umt.edu>
2017-05-26 16:48:12 -0600

    Renamed Method menuActionSoilTypeEditor_actionPerformed to soilTypeEditor to improve readabaility. Replaced ActionListener with Lambda to improve readability.

--- src/main/simpplle/gui/InvasiveSpeciesLogicDialog.java
+++ src/main/simpplle/gui/InvasiveSpeciesLogicDialog.java
@@ -12,8 +12,6 @@ import java.awt.Frame;
 import simpplle.comcode.InvasiveSpeciesLogic;
 import simpplle.comcode.SystemKnowledge;
 import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 /**
  * This class defines the Invasive Species Logic Dialog.  There are only two zones with this logic, Eastside Region 1 and Colorado Plateau.
@@ -46,8 +44,7 @@ public class InvasiveSpeciesLogicDialog extends VegLogicDialog {
 
   private void jbInit() throws Exception {
     menuActionSoilTypeEditor.setText("Soil Type Editor");
-    menuActionSoilTypeEditor.addActionListener(new
-        InvasiveSpeciesLogicDialog_menuActionSoilTypeEditor_actionAdapter(this));
+    menuActionSoilTypeEditor.addActionListener(e -> soilTypeEditor());
     menuAction.addSeparator();
     menuAction.add(menuActionSoilTypeEditor);
   }
@@ -73,21 +70,8 @@ public class InvasiveSpeciesLogicDialog extends VegLogicDialog {
     updateDialog();
   }
 
-  void menuActionSoilTypeEditor_actionPerformed(ActionEvent e) {
+  private void soilTypeEditor() {
     SoilTypeChooser dlg = new SoilTypeChooser(this,"Soil Type Editor",true,null);
     dlg.setVisible(true);
-  }
-}
-
-class InvasiveSpeciesLogicDialog_menuActionSoilTypeEditor_actionAdapter implements
-    ActionListener {
-  private InvasiveSpeciesLogicDialog adaptee;
-  InvasiveSpeciesLogicDialog_menuActionSoilTypeEditor_actionAdapter(
-      InvasiveSpeciesLogicDialog adaptee) {
-    this.adaptee = adaptee;
-  }
-
-  public void actionPerformed(ActionEvent e) {
-    adaptee.menuActionSoilTypeEditor_actionPerformed(e);
   }
 }
