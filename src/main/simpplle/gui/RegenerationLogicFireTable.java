15957908fd2f6397848041d68e06c0124e816546
Kyle <kyle.lucke@umt.edu>
2017-05-25 18:57:26 -0600

    Cleaned up RegenerationLogicFireTable.java. Removed unused imports. Privatized varaibles and methods where possible. Replaced several instantiations of Font with a single variable. Moved instantation of Layouts to JPanel constructors where possible. This is cleaner and more readable. Replaced ActionListeners with Method references.

--- src/main/simpplle/gui/RegenerationLogicFireTable.java
+++ src/main/simpplle/gui/RegenerationLogicFireTable.java
@@ -9,25 +9,14 @@
 package simpplle.gui;
 
 import java.awt.*;
-import java.io.File;
-import java.util.Enumeration;
 import java.util.Vector;
-
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
-import javax.swing.table.TableModel;
-
 import simpplle.JSimpplle;
 import simpplle.comcode.HabitatTypeGroup;
 import simpplle.comcode.FireRegenerationData;
 import simpplle.comcode.RegenerationLogic;
-import simpplle.comcode.Simpplle;
-import simpplle.comcode.SimpplleError;
 import simpplle.comcode.Species;
-import simpplle.comcode.SystemKnowledge;
 import java.awt.event.*;
 import simpplle.comcode.HabitatTypeGroupType;
 import java.awt.Font;
@@ -54,16 +43,14 @@ public class RegenerationLogicFireTable extends VegLogicPanel {
 
   private HabitatTypeGroupType selectedEcoGroup;
 
-  JPanel aPanel = new JPanel();
-  FlowLayout flowLayout2 = new FlowLayout();
-  JPanel adjacentPrefPanel = new JPanel();
-  FlowLayout flowLayout3 = new FlowLayout();
-  JButton adjacentPrefPickPB = new JButton();
-  JLabel adjacentPrefValue = new JLabel();
+  private JPanel aPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+  private JPanel adjacentPrefPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+  private JButton adjacentPrefPickPB = new JButton();
+  private JLabel adjacentPrefValue = new JLabel();
   GridLayout gridLayout1 = new GridLayout();
-  JPanel adjacentPrefLabelPanel = new JPanel();
-  JLabel adjacentPrefLabel2 = new JLabel();
-  JLabel AdjacentPrefLabel1 = new JLabel();
+  private JPanel adjacentPrefLabelPanel = new JPanel();
+  private JLabel adjacentPrefLabel2 = new JLabel();
+  private JLabel AdjacentPrefLabel1 = new JLabel();
   private JPanel ecoGroupCBPanel = new JPanel();
   private JComboBox ecoGroupCB = new JComboBox();
   private JLabel ecoGroupLabel = new JLabel();
@@ -85,39 +72,26 @@ public class RegenerationLogicFireTable extends VegLogicPanel {
     initializeBase();
   }
 
-  protected void initGUI() throws Exception {
-    aPanel.setLayout(flowLayout2);
-    flowLayout2.setAlignment(FlowLayout.LEFT);
-    flowLayout2.setHgap(0);
-    flowLayout2.setVgap(0);
-    adjacentPrefPanel.setLayout(flowLayout3);
-    flowLayout3.setAlignment(FlowLayout.LEFT);
-    flowLayout3.setHgap(10);
-    flowLayout3.setVgap(0);
+  private  void initGUI() throws Exception {
+
+    Font monospaced = new Font("Monospaced", Font.BOLD, 12);
+
     adjacentPrefPickPB.setText("Choose Species");
-    adjacentPrefPickPB.addActionListener(new java.awt.event.ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        adjacentPrefPickPB_actionPerformed(e);
-      }
-    });
+    adjacentPrefPickPB.addActionListener(this::adjacentPrefPickPB_actionPerformed);
     adjacentPrefValue.setBackground(Color.white);
-    adjacentPrefValue.setFont(new java.awt.Font("Monospaced", 1, 12));
+    adjacentPrefValue.setFont(monospaced);
     adjacentPrefValue.setForeground(Color.blue);
     adjacentPrefValue.setBorder(BorderFactory.createLoweredBevelBorder());
     adjacentPrefValue.setText("WS, BS");
     gridLayout1.setRows(2);
     adjacentPrefLabelPanel.setLayout(gridLayout1);
-    adjacentPrefLabel2.setFont(new java.awt.Font("Monospaced", 1, 12));
+    adjacentPrefLabel2.setFont(monospaced);
     adjacentPrefLabel2.setText("First on left is most preferred");
-    AdjacentPrefLabel1.setFont(new java.awt.Font("Monospaced", 1, 12));
+    AdjacentPrefLabel1.setFont(monospaced);
     AdjacentPrefLabel1.setText("Preferred Adjacent Species");
     aPanel.setBorder(BorderFactory.createEtchedBorder());
-    ecoGroupCB.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        ecoGroupCB_actionPerformed(e);
-      }
-    });
-    ecoGroupLabel.setFont(new java.awt.Font("Monospaced", Font.BOLD, 12));
+    ecoGroupCB.addActionListener(this::ecoGroupCB_actionPerformed);
+    ecoGroupLabel.setFont(monospaced);
     ecoGroupLabel.setText("Ecological Grouping");
     adjacentPrefLabelPanel.add(AdjacentPrefLabel1, null);
     adjacentPrefLabelPanel.add(adjacentPrefLabel2, null);
@@ -188,15 +162,15 @@ public class RegenerationLogicFireTable extends VegLogicPanel {
     adjacentPrefValue.setText(str);
     super.updateDialog();
   }
-/**
- * 
- */
+  /**
+   *
+   */
   public void refreshTable() {
     super.refreshTable();
     RegenerationLogic.setCurrentEcoGroup(RegenerationLogic.FIRE,selectedEcoGroup);
   }
 
-  public void addRows(Vector speciesList) {
+  void addRows(Vector speciesList) {
     int position = (selectedRow != -1) ? selectedRow : dataModel.getRowCount() + 1;
     RegenerationLogic.addDataRows(position,kind,speciesList);
     refreshTable();
@@ -229,7 +203,7 @@ public class RegenerationLogicFireTable extends VegLogicPanel {
     update(getGraphics());
   }
 
-  void adjacentPrefPickPB_actionPerformed(ActionEvent e) {
+  private void adjacentPrefPickPB_actionPerformed(ActionEvent e) {
     Vector validSpecies = HabitatTypeGroup.getValidSpecies();
     ArrayList<Species> value = RegenerationLogic.getAdjacentPreferredSpecies();
     String title = "Choose Preferred Adjacent Species";
@@ -244,7 +218,7 @@ public class RegenerationLogicFireTable extends VegLogicPanel {
     update(getGraphics());
   }
 
-  public void ecoGroupCB_actionPerformed(ActionEvent e) {
+  private  void ecoGroupCB_actionPerformed(ActionEvent e) {
     HabitatTypeGroupType selected = (HabitatTypeGroupType)ecoGroupCB.getSelectedItem();
     if (selected != selectedEcoGroup) {
       selectedEcoGroup = selected;
@@ -255,11 +229,10 @@ public class RegenerationLogicFireTable extends VegLogicPanel {
 
   }
 
-  public void setDefaultEcoGroup() {
+  void setDefaultEcoGroup() {
     ecoGroupCB.setSelectedItem(HabitatTypeGroupType.ANY);
     ecoGroupCB_actionPerformed(null);
   }
-
 }
 
 
