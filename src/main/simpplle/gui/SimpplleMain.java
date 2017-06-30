ec1d7e7a419a4d6f0babacbd0af853dfd4f4a5ae
Kyle Lucke <Kyle.Lucke@umconnect.umt.edu>
2017-06-30 15:03:28 -0600

    Incremented version number

--- src/main/simpplle/gui/SimpplleMain.java
+++ src/main/simpplle/gui/SimpplleMain.java
@@ -34,7 +34,7 @@ import java.util.Vector;
  */
 @SuppressWarnings("serial")
 public class SimpplleMain extends JFrame {
-  public static final String VERSION      = "1.3.10";
+  public static final String VERSION      = "1.3.11";
   public static final String RELEASE_KIND = "Douglas Fir";
   public static final String BUILD_DATE   = "June 2017";
 
